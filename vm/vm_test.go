6ae2f8af385461e2ce94b4e30c882a05b397f00a
thyagostall <thstall@gmail.com>
2021-07-04 09:30:40 -0300

    Booleans

--- vm/vm_test.go
+++ vm/vm_test.go
@@ -15,6 +15,15 @@ type vmTestCase struct {
 	expected interface{}
 }
 
+func TestBooleanExpressions(t *testing.T) {
+	tests := []vmTestCase{
+		{"true", true},
+		{"false", false},
+	}
+
+	runVmTests(t, tests)
+}
+
 func TestIntegerArithmetic(t *testing.T) {
 	tests := []vmTestCase{
 		{"1", 1},
@@ -68,6 +77,11 @@ func testExpectedObject(t *testing.T, expected interface{}, actual object.Object
 	t.Helper()
 
 	switch expected := expected.(type) {
+	case bool:
+		err := testBooleanObject(bool(expected), actual)
+		if err != nil {
+			t.Errorf("testBooleanObject failed: %s", err)
+		}
 	case int:
 		err := testIntegerObject(int64(expected), actual)
 		if err != nil {
@@ -84,6 +98,19 @@ func testIntegerObject(expected int64, actual object.Object) error {
 
 	if result.Value != expected {
 		return fmt.Errorf("object has wrong value. got=%d, want=%d", result.Value, expected)
+	}
+
+	return nil
+}
+
+func testBooleanObject(expected bool, actual object.Object) error {
+	result, ok := actual.(*object.Boolean)
+	if !ok {
+		return fmt.Errorf("object is not Boolean. got=%T (%+v)", actual, actual)
+	}
+
+	if result.Value != expected {
+		return fmt.Errorf("object has wrong value. got=%t, want=%t", result.Value, expected)
 	}
 
 	return nil
