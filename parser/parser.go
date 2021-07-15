0e14834fe619d718e0383bbc693f70e75b8ea6b1
thyagostall <thstall@gmail.com>
2021-07-14 21:27:13 -0300

    Fix missing semicolon issue in Let statements

--- parser/parser.go
+++ parser/parser.go
@@ -384,7 +384,7 @@ func (p *Parser) parseLetStatement() *ast.LetStatement {
 
 	stmt.Value = p.parseExpression(LOWEST)
 
-	for !p.curTokenIs(token.SEMICOLON) {
+	if p.peekTokenIs(token.SEMICOLON) {
 		p.nextToken()
 	}
 
@@ -398,7 +398,7 @@ func (p *Parser) parseReturnStatement() *ast.ReturnStatement {
 
 	stmt.ReturnValue = p.parseExpression(LOWEST)
 
-	for !p.curTokenIs(token.SEMICOLON) {
+	if p.peekTokenIs(token.SEMICOLON) {
 		p.nextToken();
 	}
 
